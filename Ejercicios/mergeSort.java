50d481a94f7640630816772f13fbe4783d8daf53
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-04-03 22:43:01 -0500

    Update mergeSort.java

--- Ejercicios/mergeSort.java
+++ Ejercicios/mergeSort.java
@@ -1,32 +1,26 @@
 public class MergeSort {
 
-    // Método principal para ordenar un arreglo
     public static void mergeSort(int[] array) {
         if (array.length <= 1) {
-            return; // El arreglo ya está ordenado
+            return; 
         }
         
-        int mid = array.length / 2;  // Encuentra el punto medio
-        int[] left = new int[mid];  // Sub-arreglo izquierdo
-        int[] right = new int[array.length - mid];  // Sub-arreglo derecho
+        int mid = array.length / 2;  
+        int[] left = new int[mid]; 
+        int[] right = new int[array.length - mid];  
 
-        // Copiar los datos en los arreglos izquierdo y derecho
         System.arraycopy(array, 0, left, 0, mid);
         System.arraycopy(array, mid, right, 0, array.length - mid);
 
-        // Ordenar los sub-arreglos recursivamente
         mergeSort(left);
         mergeSort(right);
 
-        // Fusionar los arreglos ordenados
         merge(array, left, right);
     }
 
-    // Método para fusionar dos sub-arreglos ordenados
     public static void merge(int[] array, int[] left, int[] right) {
         int i = 0, j = 0, k = 0;
 
-        // Combinamos los arreglos izquierdo y derecho
         while (i < left.length && j < right.length) {
             if (left[i] <= right[j]) {
                 array[k] = left[i];
@@ -38,14 +32,12 @@ public class MergeSort {
             k++;
         }
 
-        // Copiar los elementos restantes de left[], si los hay
         while (i < left.length) {
             array[k] = left[i];
             i++;
             k++;
         }
 
-        // Copiar los elementos restantes de right[], si los hay
         while (j < right.length) {
             array[k] = right[j];
             j++;
@@ -53,7 +45,6 @@ public class MergeSort {
         }
     }
 
-    // Método para imprimir un arreglo
     public static void printArray(int[] array) {
         for (int i : array) {
             System.out.print(i + " ");
@@ -67,7 +58,7 @@ public class MergeSort {
         System.out.println("Arreglo Original:");
         printArray(array);
 
-        mergeSort(array);  // Ordenar el arreglo
+        mergeSort(array); 
 
         System.out.println("Arreglo Ordenado:");
         printArray(array);
